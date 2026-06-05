1ee31d072c3c056eb752d6ab579095c97a259786
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-05 14:14:44 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,18 @@
+<?php
 
+$host = "sql105.infinityfree.com";
+$user = "if0_42102747";
+$pass = "YogiAkunBaru14";
+$db   = "if0_42102747_hireflow";
+
+$conn = mysqli_connect(
+    $host,
+    $user,
+    $pass,
+    $db
+);
+
+if(!$conn){
+    die("Database gagal terkoneksi");
+}
+?>
