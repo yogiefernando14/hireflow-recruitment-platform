08b499fc7f612eb89251668b1d6af4e7ab179d16
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-05 17:30:07 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -55,7 +55,36 @@ echo "<p style='color:red'>$error</p>";
 ?>
 <form method="POST">
 
+<input
+
+type="text"
+
+name="username"
+
+placeholder="Username"
+
+required>
+
+<input
+
+type="password"
+
+name="password"
+
+placeholder="Password"
+
+required>
+
+<button
+
+type="submit"
+
+name="login">
+
 Login
+
+</button>
+
 </form>
 </div>
 </body>
