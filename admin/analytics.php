28f85156273c318e65ee8cc62c77940a71119525
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-05 18:10:24 +0700

    Update analytics.php

--- admin/analytics.php
+++ admin/analytics.php
@@ -34,32 +34,32 @@ Applications Growth
 <div class="chart">
 <div class="bar-group">
 <div class="value">120</div>
-<div class="bar" style="height:60px;"></div>
+<div class="bar" style="height:50px;"></div>
 <div class="label">Jan</div>
 </div>
 <div class="bar-group">
 <div class="value">240</div>
-<div class="bar" style="height:120px;"></div>
+<div class="bar" style="height:90px;"></div>
 <div class="label">Feb</div>
 </div>
 <div class="bar-group">
 <div class="value">380</div>
-<div class="bar" style="height:180px;"></div>
+<div class="bar" style="height:130px;"></div>
 <div class="label">Mar</div>
 </div>
 <div class="bar-group">
 <div class="value">520</div>
-<div class="bar" style="height:240px;"></div>
+<div class="bar" style="height:170px;"></div>
 <div class="label">Apr</div>
 </div>
 <div class="bar-group">
 <div class="value">760</div>
-<div class="bar" style="height:320px;"></div>
+<div class="bar" style="height:210px;"></div>
 <div class="label">May</div>
 </div>
 <div class="bar-group">
 <div class="value">1248</div>
-<div class="bar" style="height:380px;"></div>
+<div class="bar" style="height:250px;"></div>
 <div class="label">Jun</div>
 </div>
 </div>
