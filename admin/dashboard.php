c8c79cf2a147d18cf99e7131ade0ffc5ee35abd0
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-05 15:06:55 +0700

    Update and rename dashboard.html to dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,16 @@
+<?php
+
+session_start();
+
+include "../config.php";
+
+$totalApplicants = mysqli_num_rows(
+
+mysqli_query($conn,"SELECT * FROM applicants")
+
+);
+
+?>
 <!DOCTYPE html>
 
 <html lang="en">
